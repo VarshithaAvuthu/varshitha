ec90ae77aac8806839bfedeac694975aee39b125
VarshithaAvuthu <varshithareddyavuthu@gmail.com>
2024-10-29 22:42:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript for handling form submission
+function submitForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (name && email && message) {
+        alert(Thank you, ${name}! Your message has been sent.);
+        document.getElementById("contactForm").reset();
+    } else {
+        alert("Please fill out all fields.");
+    }
+}
